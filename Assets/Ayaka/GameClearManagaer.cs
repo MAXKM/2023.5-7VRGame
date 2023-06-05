049d81fbd133e57a38c91ccf877c5211e5166dc6
ayaka-takano <s223116@cco.kanagawa-it.ac.jp>
2023-06-05 21:00:18 +0900

    シーン遷移のスクリプト書きました

--- Assets/Ayaka/GameClearManagaer.cs
+++ Assets/Ayaka/GameClearManagaer.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameClearManagaer : MonoBehaviour
 {
+    public float timerCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class GameClearManagaer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //3•b—§‚Á‚½‚çƒV[ƒ“‘JˆÚ
+        timerCount += Time.deltaTime;
+        if (timerCount >= 3.0f)
+        {
+            SceneChange();
+        }
         
+    }
+    void SceneChange()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 }
--- Assets/Ayaka/GameOverManager.cs
+++ Assets/Ayaka/GameOverManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
+    public float timerCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //3•b—§‚Á‚½‚çƒV[ƒ“‘JˆÚ
+        timerCount += Time.deltaTime;
+        if (timerCount >= 3.0f)
+        {
+            SceneChange();
+        }
+
+    }
+    void SceneChange()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 }
