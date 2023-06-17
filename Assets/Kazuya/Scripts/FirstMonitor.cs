79e3d2142678dd1ecb461a38d2604a73273c86dd
DESKTOP-ATM897C\kazuy <hmstmo1049@icloud.com>
2023-06-17 22:32:19 +0900

    FirstMonitorに変更を加えました

--- Assets/Kazuya/Scripts/FirstMonitor.cs
+++ Assets/Kazuya/Scripts/FirstMonitor.cs
@@ -6,11 +6,27 @@ public class FirstMonitor : MonoBehaviour
 {
     [SerializeField]GameManager gameManager;
     [SerializeField]NormalMonitorManager normalMonitorManager;
+    [SerializeField] ParticleSystem Destroy;
+    MeshRenderer meshRenderer;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         gameManager.gameStart = true;
         normalMonitorManager.monitorCount++;
+        if (meshRenderer == null)
+        {
+            meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        }
+        meshRenderer.enabled = false;
+        Destroy.Play();
+        StartCoroutine(HideCoroutine());
+
+    }
+    WaitForSeconds hideWait = new WaitForSeconds(0.7f);
+
+    IEnumerator HideCoroutine()
+    {
+        yield return hideWait;
         normalMonitorManager.ReturnObjectToPool(this.gameObject);
     }
 }
