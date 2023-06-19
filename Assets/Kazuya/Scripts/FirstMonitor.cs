0895cb9b3d78b26e49b7002c514bf4fd096635f9
DESKTOP-ATM897C\kazuy <hmstmo1049@icloud.com>
2023-06-19 21:45:43 +0900

    FirstMonitorに関して変更を加えました

--- Assets/Kazuya/Scripts/FirstMonitor.cs
+++ Assets/Kazuya/Scripts/FirstMonitor.cs
@@ -7,15 +7,36 @@ public class FirstMonitor : MonoBehaviour
     [SerializeField]GameManager gameManager;
     [SerializeField]NormalMonitorManager normalMonitorManager;
     [SerializeField] ParticleSystem Destroy;
+    HandDetection handdetection;
+    [SerializeField]SkillManager skillmanager;
     MeshRenderer meshRenderer;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        Vector3 contactPoint = other.ClosestPoint(transform.position);
         gameManager.gameStart = true;
         //normalMonitorManager.monitorCount++;
         if (meshRenderer == null)
         {
             meshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        }
+        
+        if (other.gameObject.tag == "LeftHand")
+        {
+            if (handdetection == null)
+            {
+                GameObject obj = GameObject.FindGameObjectWithTag("RightHand");
+                handdetection = obj.GetComponent<HandDetection>();
+            }
+            skillmanager.DDamage(contactPoint, handdetection.distanceLeft);
+        }
+        else if (other.gameObject.tag == "RightHand")
+        {
+            if (handdetection == null)
+            {
+                handdetection = other.gameObject.GetComponent<HandDetection>();
+            }
+            skillmanager.DDamage(contactPoint, handdetection.distanceRight);
         }
         meshRenderer.enabled = false;
         Destroy.Play();
