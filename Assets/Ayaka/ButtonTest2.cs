20ae0d72377ec1b6eb16440d40ef10500b9959e1
Numachi0622 <122448310+Numachi0622@users.noreply.github.com>
2023-06-23 22:26:39 +0900

    たかあやさんのスクリプト名を変更

--- Assets/Ayaka/ButtonTest2.cs
+++ Assets/Ayaka/ButtonTest2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Buttonatest : MonoBehaviour
+public class ButtonTest2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
